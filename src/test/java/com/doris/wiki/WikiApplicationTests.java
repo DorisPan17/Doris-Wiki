c3ff96a54b8b9cf007435d62c90d077873f7bb3c
Startty17 <startty17@163.com>
2023-10-05 17:36:23 -0700

    2-2 init

---
+++ src/test/java/com/doris/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doris.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
