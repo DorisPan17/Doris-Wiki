445995192e8f20984e650aee1d43ce00a0593478
Startty17 <startty17@163.com>
2023-10-14 17:11:57 -0700

    2-6 Develop a Hello World API. Access address: http://127.0.0.1:8880/hello

--- src/main/java/com/doris/wiki/WikiApplication.java
+++ src/main/java/com/doris/wiki/WikiApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+// @ComponentScan({"com.jiawa", "com.test"})
+@ComponentScan("com.doris")
 @SpringBootApplication
 public class WikiApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
