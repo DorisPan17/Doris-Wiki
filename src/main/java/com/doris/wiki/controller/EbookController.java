3953cb8ad732863d22ae785d44ca8bc781eda575
Startty17 <startty17@163.com>
2023-11-04 23:55:47 -0700

    3-6 Add a common response class CommonResp. All responses from backend interfaces will be of type CommonResp from now on 增加通用的返回类CommonResp,以后所有后端接口的返回志全部都是CommonResp类型

--- src/main/java/com/doris/wiki/controller/EbookController.java
+++ src/main/java/com/doris/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.doris.wiki.controller;
 
 import com.doris.wiki.domain.Ebook;
+import com.doris.wiki.resp.CommonResp;
 import com.doris.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,10 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
     @GetMapping("/list1")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
