da444aafddcd1b408c71f1828e20ceb26e2f0362
Startty17 <startty17@163.com>
2023-11-30 21:20:42 -0800

    6-3 + 6-4 Displaying the eBook table, integrate the PageHelper plugin.电子书表格展示，集成PageHelper插件

--- src/main/java/com/doris/wiki/service/EbookService.java
+++ src/main/java/com/doris/wiki/service/EbookService.java
@@ -6,10 +6,13 @@ import com.doris.wiki.mapper.EbookMapper;
 import com.doris.wiki.req.EbookReq;
 import com.doris.wiki.resp.EbookResp;
 import com.doris.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +20,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -27,7 +32,13 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
